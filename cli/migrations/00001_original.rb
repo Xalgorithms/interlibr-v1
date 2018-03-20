66c53fe7b55b14372f8ab52be18fae2af1bce1d3
Don Kelly <karfai@gmail.com>
2018-03-20 22:54:31 +0400

    When migrating, only drop the keyspace if it exists
    Change-Id: I89c77947004d971ff49a21cbca4f6c7213e34b9e

--- cli/migrations/00001_original.rb
+++ cli/migrations/00001_original.rb
@@ -3,7 +3,7 @@ require_relative '../support/simple_migration'
 class Original < Support::SimpleMigration
   def self.statements
     [
-      'DROP KEYSPACE xadf',
+      'DROP KEYSPACE IF EXISTS xadf',
       "CREATE KEYSPACE xadf WITH REPLICATION =  { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }",
       'CREATE TABLE xadf.when_keys (section text, key text, PRIMARY KEY (section, key))',
       'CREATE TABLE xadf.whens (section text, key text, op text, val text, rule_id text, PRIMARY KEY (rule_id, section, key))',
