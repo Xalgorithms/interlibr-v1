32815c067f1c5320565f7c80bf8fe255c6ff8b5f
Don Kelly <karfai@gmail.com>
2018-07-14 09:57:07 +0400

    Sorts the step results returned by the query API
    There is no guarantee that the steps in a trace query are ordered. For
    the last_step functionality, they need to be sorted according to
    index, phase.

--- cli/clients/query.rb
+++ cli/clients/query.rb
@@ -13,7 +13,13 @@ module Clients
 
     def last_step_by_request(req_id)
       resp = @conn.get("/requests/#{req_id}/traces")
-      resp.status == 200 ? resp.body.first["steps"].last : nil
+      resp.status == 200 ? resp.body.first["steps"].sort do |a, b|
+                             if a['index'] != b['index']
+                               a['index'] <=> b['index']
+                             else
+                               b['phase'] == 'start' ? 1 : -1
+                             end
+                           end.last : nil
     end
   end    
 end
