3147dd216dd2c1b1fa1f51cc6e30c989d607082b
Don Kelly <karfai@gmail.com>
2018-08-23 13:42:06 +0400

    Removes old Mesos deployment

--- mesos/kafka-topics.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dcos package install kafka --cli --yes
-dcos kafka topic create xadf.compute.documents --partitions 1 --replication 1
-dcos kafka topic create xadf.compute.effective --partitions 1 --replication 1
-dcos kafka topic create xadf.compute.applicable --partitions 1 --replication 1
-dcos kafka topic create xadf.compute.revision --partitions 1 --replication 1
