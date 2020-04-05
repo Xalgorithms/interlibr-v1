46764e2222cc71bd3cfe78122ad2fb1ef8781728
Don Kelly <karfai@gmail.com>
2020-04-05 06:52:58 -0400

    Upgrades CLI to ruby@2.7

--- cli/publish.sh
+++ cli/publish.sh
@@ -3,7 +3,7 @@ VERSION=`cat .version`
 ENVIRONMENTS="development production"
 
 for en in $ENVIRONMENTS; do
-    docker build --squash -t "xalgorithms/il-cli:latest-$en" -t "xalgorithms/il-cli:$VERSION-$en" -f "Dockerfile.$en" .
+    docker build -t "xalgorithms/il-cli:latest-$en" -t "xalgorithms/il-cli:$VERSION-$en" -f "Dockerfile.$en" .
     docker push "xalgorithms/il-cli:latest-$en"
     docker push "xalgorithms/il-cli:$VERSION-$en"
 done
